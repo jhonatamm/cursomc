fac378c7d0829f324d0c2061958a41268b77bdc4
Jhonatam Medina <jhonatam.forever@gmail.com>
2018-07-27 16:46:56 -0300

    Classe de Dominio - testando e criando

--- src/main/java/com/medina/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/medina/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.medina.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.medina.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return " Rest esta funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1,"informatica");
+		Categoria c2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> clist = new ArrayList<>();
+		clist.add(c1);
+		clist.add(c2);
+		
+		return clist;
 	}
 
 }
