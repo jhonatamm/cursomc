d318cc0644e8c0299ea128aa72b95f1e608aecbe
Jhonatam Medina <jhonatam.forever@gmail.com>
2018-08-18 22:20:12 -0300

    inserindo uma nova categoria com post

--- src/main/java/com/medina/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/medina/cursomc/resources/CategoriaResource.java
@@ -1,12 +1,16 @@
 package com.medina.cursomc.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.medina.cursomc.domain.Categoria;
 import com.medina.cursomc.services.CategoriaService;
@@ -27,6 +31,14 @@ public class CategoriaResource {
 	@RequestMapping(value="/test", method= RequestMethod.GET)
 	public String test() {
 		return "ok ok ok ok ok";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void>   insert(@RequestBody Categoria obj){
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
