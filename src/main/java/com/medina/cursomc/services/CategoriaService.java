5b39364da93a0552006aca30a7473fb3d5cca999
Jhonatam Medina <jhonatam.forever@gmail.com>
2018-08-23 15:25:34 -0300

    Validação Sintatica

--- src/main/java/com/medina/cursomc/services/CategoriaService.java
+++ src/main/java/com/medina/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.medina.cursomc.domain.Categoria;
+import com.medina.cursomc.dto.CategoriaDTO;
 import com.medina.cursomc.repositories.CategoriaRepository;
 import com.medina.cursomc.services.exceptions.DataIntegrityException;
 import com.medina.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,6 +59,11 @@ public class CategoriaService {
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		
+		return new Categoria(objDto.getId(),objDto.getNome());
+		
 	}
 
 }
