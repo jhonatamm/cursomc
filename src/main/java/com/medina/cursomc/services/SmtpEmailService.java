e3dd5aa8aa7d2f688d521c560a63a30d1a82d707
Jhonatam Medina <jhonatam.forever@gmail.com>
2018-09-06 19:42:37 -0300

    Enviando email servidor google

---
+++ src/main/java/com/medina/cursomc/services/SmtpEmailService.java
@@ -0,0 +1,26 @@
+package com.medina.cursomc.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class SmtpEmailService extends AbstractEmailService {
+	
+	@Autowired private MailSender mailSender;
+	
+	private static final Logger log = LoggerFactory.getLogger(SmtpEmailService.class);
+
+
+	@Override
+	public void sendEmail(SimpleMailMessage msg) {
+		// TODO Auto-generated method stub
+		
+		log.info("Enviando email");
+		mailSender.send(msg);
+		log.info("Email Enviado");
+
+	}
+
+}
